619764c0083864d29f4b21f491c45295e19cf08d
liekepieterse <145223392+liekepieterse@users.noreply.github.com>
2024-11-28 16:49:50 +0100

    28-11-2024

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,13 @@ hamburgerStreepjes.addEventListener('click', () =>{
   hamburgerMenu.classList.toggle('active');
 })
 //  bron hamburger menu: https://youtu.be/aNDqzlAKmZc?si=_QBkHuhMWLj0btXJ
+
+
+const filter= document.querySelector('main button:nth-of-type(1)');
+
+const uitgeklaptFilter = document.querySelector('main ul:nth-of-type(2)');
+
+filter.addEventListener('click', () =>{
+  filter.classList.toggle('active');
+  uitgeklaptFilter.classList.toggle('active');
+})
