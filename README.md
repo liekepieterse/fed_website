d8872bdfafff7d8c32ddb5670d60b378f9bf2e99
liekepieterse <145223392+liekepieterse@users.noreply.github.com>
2024-12-04 01:07:53 +0100

    4-12-2024

--- README.md
+++ README.md
@@ -148,20 +148,20 @@ Font toepassen met @fontface is nog niet gelukt
   ### Agenda voor meeting
   samen met je groepje opstellen
 
-  | student 1      | student 2          | student 3    | student 4        |
-  | ---            | ---                | ---          | ---              |
-  | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
-  | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
-  | ...            | ...                | ...          | ...              |
+  student 1               
+Er mist een span in mijn hamburgermenu.
+De h1 krijg ik niet verticaal ge centreerd op de video.
+De pijltjes bij de details krijg ik niet weg.
+De linkjes in de tekst onderaan de pagina staan raar.
 
 
   ### Verslag van meeting
   hier na afloop snel de uitkomsten van de meeting vastleggen
 
-  - punt 1
-  - punt 2
-  - nog een punt
-- ...
+   -voor de hamburger moest ik de nth-child aanpassen naar nth-of-type en de top % van de tweede span aanpassen.
+  -voor de header was het probleem dat de section waar de tekst in staat niet even groot is als de video. De manier om dit op t elossen is door de heigth van de section aan te passen.
+  -Voor de pijltjes bij de details die ik niet weg kreeg is de oplossing de list-style-type none op de summary te zetten in plaats van de details.
+  -voor de linkjes in de footer tekst stond er een algemene p met display flex waardoor hij alles op een nieuwe regel neerzette.
 
 </details>
 
@@ -205,10 +205,7 @@ Font toepassen met @fontface is nog niet gelukt
   ### Verslag van meeting
   hier na afloop snel de uitkomsten van de meeting vastleggen
 
-  -voor de hamburger moest ik de nth-child aanpassen naar nth-of-type en de top % van de tweede span aanpassen.
-  -voor de header was het probleem dat de section waar de tekst in staat niet even groot is als de video. De manier om dit op t elossen is door de heigth van de section aan te passen.
-  -Voor de pijltjes bij de details die ik niet weg kreeg is de oplossing de list-style-type none op de summary te zetten in plaats van de details.
-  -voor de linkjes in de footer tekst stond er een algemene p met display flex waardoor hij alles op een nieuwe regel neerzette.
+ 
 
 </details>
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,82 @@
 // JavaScript Document
 console.log("hi");
 
-const hamburgerStreepjes= document.querySelector('nav button');
 
-const hamburgerMenu = document.querySelector('ul');
+// Hmaburger menu
 
-hamburgerStreepjes.addEventListener('click', () =>{
-  hamburgerStreepjes.classList.toggle('active');
-  hamburgerMenu.classList.toggle('active');
-})
+const hamburgerStreepjes = document.querySelector("nav button");
+
+const hamburgerMenu = document.querySelector("ul");
+
+hamburgerStreepjes.addEventListener("click", () => {
+  hamburgerStreepjes.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+});
 //  bron hamburger menu: https://youtu.be/aNDqzlAKmZc?si=_QBkHuhMWLj0btXJ
 
 
-const filter= document.querySelector('main button:nth-of-type(1)');
-
-const uitgeklaptFilter = document.querySelector('main ul:nth-of-type(2)');
-
-const closeButton = document.querySelector('main ul:nth-of-type(2) li:nth-of-type(1) img')
-
-const bekijkProducten = document.querySelector('main ul:nth-of-type(2) button:nth-of-type(1)')
-
-filter.addEventListener('click', () => {
-  uitgeklaptFilter.classList.toggle('active');
-})
-
-closeButton.addEventListener('click', ()=> {
-  uitgeklaptFilter.classList.remove('active');
-})
-
-closeButton.addEventListener('click', ()=> {
-  uitgeklaptFilter.classList.remove('active');
-})
-
-bekijkProducten.addEventListener('click', ()=> {
-  uitgeklaptFilter.classList.remove('active');
-})
 
 
 
-const sorteer= document.querySelector('main button:nth-of-type(2)');
+// filter knop
+const filter = document.querySelector("main button:nth-of-type(1)");
 
-const uitgeklaptSorteer = document.querySelector('main ul:nth-of-type(3)');
+const uitgeklaptFilter = document.querySelector("main ul:nth-of-type(1)");
 
-const closeSorteer = document.querySelector('main ul:nth-of-type(3) li:nth-of-type(1) img')
+const closeButton = document.querySelector(
+  "main ul:nth-of-type(1) li:nth-of-type(1) img"
+);
 
-const bekijksorteer = document.querySelector('main ul:nth-of-type(3) button:nth-of-type(1)')
+const bekijkProducten = document.querySelector(
+  "main ul:nth-of-type(1) button:nth-of-type(1)"
+);
 
-sorteer.addEventListener('click', () => {
-  uitgeklaptSorteer.classList.toggle('active');
-})
+filter.addEventListener("click", () => {
+  uitgeklaptFilter.classList.toggle("active");
+});
 
-closeSorteer.addEventListener('click', ()=> {
-  uitgeklaptSorteer.classList.remove('active');
-})
+closeButton.addEventListener("click", () => {
+  uitgeklaptFilter.classList.remove("active");
+});
 
-closeSorteer.addEventListener('click', ()=> {
-  uitgeklaptSorteer.classList.remove('active');
-})
+closeButton.addEventListener("click", () => {
+  uitgeklaptFilter.classList.remove("active");
+});
 
-bekijksorteer.addEventListener('click', ()=> {
-  uitgeklaptSorteer.classList.remove('active');
-})
+bekijkProducten.addEventListener("click", () => {
+  uitgeklaptFilter.classList.remove("active");
+});
+
+
+
+
+
+
+// sorteer knop
+const sorteer = document.querySelector("main button:nth-of-type(2)");
+
+const uitgeklaptSorteer = document.querySelector("main ul:nth-of-type(2)");
+
+const closeSorteer = document.querySelector(
+  "main ul:nth-of-type(2) li:nth-of-type(1) img"
+);
+
+const bekijksorteer = document.querySelector(
+  "main ul:nth-of-type(2) button:nth-of-type(1)"
+);
+
+sorteer.addEventListener("click", () => {
+  uitgeklaptSorteer.classList.toggle("active");
+});
+
+closeSorteer.addEventListener("click", () => {
+  uitgeklaptSorteer.classList.remove("active");
+});
+
+closeSorteer.addEventListener("click", () => {
+  uitgeklaptSorteer.classList.remove("active");
+});
+
+bekijksorteer.addEventListener("click", () => {
+  uitgeklaptSorteer.classList.remove("active");
+});
